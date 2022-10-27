39e412c2e3d2b15f5438a2626f33abc9c08d1083
Jordan Urbaez Lu <jordana.urbaez@gmail.com>
2022-10-26 21:03:34 -0400

    configuration

---
+++ public/index.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+
+const Home = ({}) => {
+  return (
+    <div className="snap-y snap-mandatory h-screen overflow-y-scroll scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 bg-[rgb(36,36,36)] text-white z-0">
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
